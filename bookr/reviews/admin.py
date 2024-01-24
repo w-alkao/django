f90ac4d1d4baef59e46717f430b5c182b7ab03c9
w-alkao <walk.adamou@gmail.com>
2024-01-24 10:00:44 +0100

    Jan 24

--- bookr/reviews/admin.py
+++ bookr/reviews/admin.py
@@ -4,20 +4,42 @@ from reviews.models import Publisher, Contributor, Book, BookContributor, Review
 # Register your models here
 
 class BookAdmin(admin.ModelAdmin):
-  list_display = ('title', 'isbn13')
+  date_hierarchy = 'publication_date'
+  list_display = ('title', 'isbn13', 'has_isbn')
+  list_filter = ('publisher', 'publication_date')
+  search_fields = ('title', 'isbn', 'publisher__name')
+  @admin.display(
+    ordering='isbn',
+    description='ISBN-13',
+    empty_value='-/',
+  )
   def isbn13(self, obj):
     return f'{obj.isbn[0:3]}-{obj.isbn[3:4]}-{obj.isbn[4:6]}-{obj.isbn[6:12]}-{obj.isbn[12:13]}'
 
+  @admin.display(
+    boolean=True,
+    description='Has ISBN-13'
+  )
+  def has_isbn(self, obj):
+    return bool(obj.isbn)
 
-def initialled_name(obj):
-  initials = ''.join([name[0] for name in obj.first_names.split(' ')])
-  return f'{obj.last_names}, {initials}'
 
 class ContributorAdmin(admin.ModelAdmin):
-  list_display = (initialled_name,)
+  list_display = ('last_names', 'first_names',)
+  search_fields = ('last_names', 'first_names')
+  list_filter = ('last_names',)
+
+
+class ReviewAdmin(admin.ModelAdmin):
+  exclude = ('date_edited',)
+  #fields = ('content', 'rating', 'creator', 'book')
+  fieldsets = (
+    (None, {'fields': ('creator', 'book')}),
+    ('Review content', {'fields': ('content', 'rating')})
+  )
 
 admin.site.register(Publisher)
 admin.site.register(Contributor, ContributorAdmin)
 admin.site.register(Book, BookAdmin)
 admin.site.register(BookContributor)
-admin.site.register(Review)
+admin.site.register(Review, ReviewAdmin)
--- bookr/reviews/models.py
+++ bookr/reviews/models.py
@@ -35,6 +35,9 @@ class Contributor(models.Model):
   last_names = models.CharField(max_length=50, help_text="The contributor's last name or names.")
   email = models.EmailField(help_text="The contact email for the contributor.")
 
+  def initialled_name(self):
+    initials = ''.join(name[0] for name in self.first_names.split(' '))
+    return f'{self.last_names}, {initials}'
   def __str__(self):
     return self.first_names
 
