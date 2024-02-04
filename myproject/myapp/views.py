7a6a7742ff91b19f944be0fd8d868e23325a8b1b
w-alkao <walk.adamou@gmail.com>
2024-02-04 19:59:44 +0100

    Feb 04

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def form_example(request):
+  for name in request.POST:
+    print(f"{name}: {request.POST.getlist(name)}")
+
+  return render(request, "form-example.html", {"method": request.method})
--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -17,8 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.views.generic import TemplateView
+import myapp.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', TemplateView.as_view(template_name='index.html'))
+    path('', TemplateView.as_view(template_name='index.html')),
+    path('form-example/', myapp.views.form_example),
 ]
