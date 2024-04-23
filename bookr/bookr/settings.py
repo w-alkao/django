3fa0289897d43483fa4ec8272d7b2f0d1f5c349f
w-alkao <walk.adamou@gmail.com>
2024-04-23 23:27:09 +0100

    image and PDF upload of books

--- bookr/bookr/settings.py
+++ bookr/bookr/settings.py
@@ -125,3 +125,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATICFILES_DIRS = [BASE_DIR / "static"]
+
+MEDIA_ROOT = BASE_DIR / "media"
+
+MEDIA_URL = "/media"
