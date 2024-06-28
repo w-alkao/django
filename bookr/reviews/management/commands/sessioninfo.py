7dedb82a9095b361797f68af7b2fb1092ff20f11
w-alkao <walk.adamou@gmail.com>
2024-06-28 18:45:27 +0100

    Sessions and authentication, examining the session key.

---
+++ bookr/reviews/management/commands/sessioninfo.py
@@ -0,0 +1,9 @@
+from pprint import pformat
+from django.contrib.auth.models import User
+from django.contrib.sessions.models import Session
+from django.contrib.sessions.backends.db import SessionStore
+from django.core.management.base import BaseCommand
+
+
+
+
