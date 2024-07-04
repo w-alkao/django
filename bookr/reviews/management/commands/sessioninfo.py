12ae9bff8ae4fe9977c894506cced7ba6f40ab7e
w-alkao <walk.adamou@gmail.com>
2024-07-04 14:01:57 +0100

    Sessions and authentication, examining the session key.

--- bookr/reviews/management/commands/sessioninfo.py
+++ bookr/reviews/management/commands/sessioninfo.py
@@ -5,5 +5,18 @@ from django.contrib.sessions.backends.db import SessionStore
 from django.core.management.base import BaseCommand
 
 
-
+class Command(BaseCommand):
+	help = "List all user sessions and the data that they contain."
+	
+	def handle(self, *args, **options):
+		session_store = SessionStore()
+		for session in Session.objects.all():
+			data = session_store.decode(session.session_data)
+			user = User.objects.get(id=data['_auth_user_id'])
+			self.stdout.write(
+				f"Session Key: {session.session_key}"
+				f"User: {user.id} {user.username}"
+				f" {user.email}"
+			)
+			self.stdout.write(pformat(data))
 
